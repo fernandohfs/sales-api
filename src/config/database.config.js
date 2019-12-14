2e5f84a1aa27b4fc22930d8bb2ebf9ca0d4f7a9c
Fernando Soares <fernando.soares@luizalabs.com>
2019-12-14 16:07:54 -0300

    added database configuration

---
+++ src/config/database.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "mysql",
+  host: "localhost",
+  username: "root",
+  password: "root1234",
+  database: "sales",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
