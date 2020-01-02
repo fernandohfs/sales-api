f1418b99a3d5b99326d7bbc032b8ec2d65a96468
Gabriel <gabrielborc@gmail.com>
2020-01-02 01:19:25 -0300

    changed: configured the database environments

--- src/config/database.migrations.config.js
+++ src/config/database.migrations.config.js
@@ -1,18 +1,46 @@
 require('dotenv').config();
 const Env = require('./environment.config');
 
-isTrueSetDebug  = (Env.DEBUG === 'true');
+isTrueSetDebug = (Env.DEBUG === 'true');
 
 module.exports = {
-  dialect: Env.DB_DIALECT,
-  host: Env.DB_HOST,
-  username: Env.DB_USERNAME,
-  password: Env.DB_PASSWORD,
-  database: Env.DB_NAME,
-  logging: isTrueSetDebug,
-  define: {
-    timestamps: true,
-    underscored: true,
-    underscoredAll: true,
-  },
+    development: {
+        dialect: Env.DB_DIALECT,
+        host: Env.DB_HOST,
+        username: Env.DB_USERNAME,
+        password: Env.DB_PASSWORD,
+        database: (/.*\_dev/.test(Env.DB_NAME) ? Env.DB_NAME : `${Env.DB_NAME}_dev`),
+        logging: isTrueSetDebug,
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        }
+    },
+    test: {
+        dialect: Env.DB_DIALECT,
+        host: Env.DB_HOST,
+        username: Env.DB_USERNAME,
+        password: Env.DB_PASSWORD,
+        database: (/.*\_test/.test(Env.DB_NAME) ? Env.DB_NAME : `${Env.DB_NAME}_test`),
+        logging: isTrueSetDebug,
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        }
+    },
+    production: {
+        dialect: Env.DB_DIALECT,
+        host: Env.DB_HOST,
+        username: Env.DB_USERNAME,
+        password: Env.DB_PASSWORD,
+        database: Env.DB_NAME,
+        logging: isTrueSetDebug,
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        }
+    }
 };
